24d8728219bc0404f939b42039586b88ee5fe366
handsomehan <handsomehan515@gmail.com>
2022-10-10 10:26:27 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Game from './pages/Game';
 import Room from './pages/Room';
 import NotFound from './pages/NotFund';
 
-function App () {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/game" element={<Game />} />
-          <Route path="/" element={<Navigate to="/game" replace />} />
-          <Route path='/room/:roomId/user/:userId' element={<Room />}/>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+function App() {
+	return (
+		<Router>
+			<div>
+				<Routes>
+					<Route path="/game" element={<Game />} />
+					<Route path="/" element={<Navigate to="/game" replace />} />
+					<Route path="/room/:roomId/user/:userId" element={<Room />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
